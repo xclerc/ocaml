0fcd0fb7d5216d1edc8e0497eff5e6d9e7a5fd3b
Mark Shinwell <mshinwell@gmail.com>
2016-11-11 17:28:53 +0000

    work

--- middle_end/lift_let_cont.ml
+++ middle_end/lift_let_cont.ml
@@ -34,12 +34,14 @@ type thing_to_lift =
 let rec find_things_to_lift (acc : thing_to_lift list) (expr : Flambda.t) =
   match expr with
   | Let_cont { name; body = body1; handler = Handler {
-        params; recursive; handler = (Let_cont { name = name'; body = _;
+        params; recursive; handler = (Let_cont { name = _ (* name' *); body = _;
           handler = Handler { handler; _ }; _ }) as let_cont2; }; }
       when params_do_not_overlap_free_variables ~params ~handler ->
+(*
 Format.eprintf "lifting continuation %a nested inside %a\n%!"
   Continuation.print name'
   Continuation.print name;
+*)
     (* The continuation [handler] can be lifted.  First we try to lift things
        (recursively) from inside [handler] itself and then (on the way back
        up from that recursion) we add the lifted continuations to [acc]. *)
@@ -60,8 +62,10 @@ Format.eprintf "lifting continuation %a nested inside %a\n%!"
         params; handler = Let let_expr; }; }
       when params_do_not_overlap_free_variables_let ~params ~let_expr
         && Effect_analysis.no_effects_named let_expr.defining_expr ->
+(*
 Format.eprintf "lifting let %a nested inside %a\n%!" Variable.print let_expr.var
   Continuation.print name;
+*)
     (* The let-bound expression [let_expr] can be lifted.  Since there cannot
        be any nested [Let] or [Let_cont] in the defining expression of the
        [Let], we don't need to recurse into that expression.
@@ -76,14 +80,12 @@ Format.eprintf "lifting let %a nested inside %a\n%!" Variable.print let_expr.var
     acc, lift body
   | Let_cont { name; body; handler = Handler {
       params; recursive; handler; }; } ->
+(*
 Format.eprintf "not lifting continuation %a\n%!" Continuation.print name;
+*)
     (* The continuation [handler] cannot be lifted---but there might be
        sequences of [Let_cont]s or [Let]s inside [handler] that can be lifted
-       (without bringing them out of [handler]).  First we deal with those.
-       At that point, observe that there cannot be any non-lifted [Let_cont]s
-       (or [Let]s) between this [Let_cont] and the (outer) [Let] or [Let_cont]
-       where we started lifting.  By virtue of this property we can just add
-       the [Let_cont] to [acc] without breaking any scoping rules. *)
+       (without bringing them out of [handler]). *)
     let handler = lift handler in
     let acc =
       (Continuation (name, Handler { params; recursive; handler; })) :: acc
