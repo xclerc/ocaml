d67faf4fa6eeb0f81d7a33eec2c0cf2c1a8966b4
Mark Shinwell <mshinwell@janestreet.com>
2016-11-15 14:15:53 +0000

    inlining

--- middle_end/lift_let_cont.ml
+++ middle_end/lift_let_cont.ml
@@ -14,116 +14,247 @@
 (*                                                                        *)
 (**************************************************************************)
 
+(* CR mshinwell: We should also do:
+     let x = <named> in
+     let_cont k = <handler> in
+     <body>
+   -->
+     let_cont k =
+       let x = ... in <handler>
+     in
+     <body>
+
+  when <named> has only generative effects and x is not free in <body>.
+*)
+
 [@@@ocaml.warning "+a-4-9-30-40-41-42"]
 
-let params_do_not_overlap_free_variables ~params ~handler =
-  let params = Variable.Set.of_list params in
-  let handler_fvs = Flambda.free_variables handler in
-  Variable.Set.is_empty (Variable.Set.inter params handler_fvs)
-
-let _params_do_not_overlap_free_variables_let ~params
-      ~(let_expr : Flambda.let_expr) =
-  let params = Variable.Set.of_list params in
-  let defining_expr_fvs = let_expr.free_vars_of_defining_expr in
-  Variable.Set.is_empty (Variable.Set.inter params defining_expr_fvs)
-
 type thing_to_lift =
-  | Continuation of Continuation.t * Flambda.let_cont_handler
   | Let of Variable.t * Flambda.named
   | Let_mutable of Mutable_variable.t * Variable.t * Lambda.value_kind
+  | Let_cont of Continuation.t * Flambda.let_cont_handler
+  | Terminator of Flambda.expr
 
-(* CR mshinwell: We should review to make sure lifting Const lets is
-   the right thing to do.  Leo pointed out what we could also push
-   non side effecting lets (e.g. makeblocks) downwards into let_cont
-   handlers when the bound var isn't used in the body of such let_cont. *)
+module Node = struct
+  include Identifiable.Make (struct
+    type t =
+      | Root
+      | Variable of Variable.t
+      | Continuation of Continuation.t
+      | Terminator of Flambda.expr
 
-let rec find_things_to_lift (acc : thing_to_lift list) (expr : Flambda.t) =
-  match expr with
-  | Let_cont { name; body = body1; handler = Handler {
-        params; recursive; handler = (Let_cont { name = _ (* name' *); body = _;
-          handler = Handler { handler; _ }; _ }) as let_cont2; }; }
-      when params_do_not_overlap_free_variables ~params ~handler ->
-    (* The continuation [handler] can be lifted.  First we try to lift things
-       (recursively) from inside [handler] itself.  (The recursive call will
-       be the thing that actually adds [handler] to [acc].) *)
-    let acc, body2 = find_things_to_lift acc let_cont2 in
-    let acc =
-      (Continuation (name, Handler {
-          params; recursive; handler = body2; }))
-        :: acc
+    let compare t1 t2 =
+      match t1, t2 with
+      | Variable v1, Variable v2 -> Variable.compare v1 v2
+      | Continuation c1, Continuation c2 -> Continuation.compare c1 c2
+      | Variable _, Continuation _ -> (-1)
+      | Continuation _, Variable _ -> 1
+
+    let equal t1 t2 =
+      compare t1 t2 = 0
+
+    let hash t =
+      match t with
+      | Variable v -> Hashtbl.hash (0, Variable.hash v)
+      | Continuation c -> Hashtbl.hash (1, Continuation.hash c)
+
+    let output _chan _t = Misc.fatal_error "not implemented"
+    let print _ppf _t = Misc.fatal_error "not implemented"
+  end)
+
+  let set_of_variable_set vs =
+    Variable.Set.fold (fun var depends_on ->
+        Set.add (Variable var) depends_on)
+      vs
+      Node.Set.empty
+
+  let set_of_continuation_set vs =
+    Continuation.Set.fold (fun cont depends_on ->
+        Set.add (Continuation cont) depends_on)
+      vs
+      Node.Set.empty
+
+  let of_thing (thing : thing_to_lift) =
+    match thing with
+    | Let (var, _) -> Variable var
+    | Let_mutable ...
+    | Let_cont (cont, _) -> Continuation cont
+    | Terminator expr -> Terminator expr
+end
+
+let add_to_graph (graph, definitions) (thing : thing_to_lift) ~depends_on =
+  let target_node = Node.of_thing thing in
+  let depends_on =
+    if Node.Set.is_empty depends_on then Node.Set.singleton Root
+    else depends_on
+  in
+  let graph =
+    Node.Set.fold (fun source_node graph ->
+        let target_nodes =
+          match Node.Map.find source_node graph with
+          | exception Not_found -> Node.Set.empty
+          | target_nodes -> target_nodes
+        in
+        Node.Map.add source_node (Node.Set.add target_node target_nodes)
+          graph)
+      depends_on
+      graph
+  in
+  let definitions = Node.Map.add target_node thing definitions in
+  graph, definitions
+
+let topological_sort (graph, definitions) =
+  let rec traverse_node node =
+    let children = Node.Map.find node graph in
+    let results =
+      Node.Set.fold (fun child results ->
+          (traverse_node child) :: results)
+        children
+        []
     in
-    acc, lift body1
-  | Let_cont { name; body; handler = Handler {
-      handler = Let_cont { handler = Alias alias_to; _ }; }; } ->
-    (* Continuation aliases nested immediately inside another [Let_cont]'s
-       handler can always be lifted. *)
-    let acc = (Continuation (name, Alias alias_to)) :: acc in
-    acc, lift body
-(*
-  | Let_cont { name; body; handler = Handler {
-        params; handler = Let let_expr; }; }
-      when params_do_not_overlap_free_variables_let ~params ~let_expr
-        && begin match let_expr.defining_expr with
-           | Const _ -> true
-           | Var _ | Prim _ | Assign _ | Read_mutable _ | Symbol _
-           | Read_symbol_field _ | Allocated_const _ | Set_of_closures _
-           | Project_closure _ | Move_within_set_of_closures _ | Project_var _
-           | Proved_unreachable -> false
-           end ->
-    (* The let-bound expression [let_expr] can be lifted.  Since there cannot
-       be any nested [Let] or [Let_cont] in the defining expression of the
-       [Let], we don't need to recurse into that expression.
-       Lifting let-bound expressions in this manner allows us to lift more
-       continuations (that may depend on such lets). *)
-    let acc, body2 = find_things_to_lift acc (Flambda.Let let_expr) in
-    let acc =
-      (Continuation (name, Handler {
-          params; recursive = Nonrecursive; handler = body2; }))
-        :: acc
+    node :: List.concat results
+  in
+  match Node.Map.find root graph with
+  | exception Not_found -> Misc.fatal_error "Graph has no root node"
+  | _ ->
+    let results = traverse_node Root children [] in
+    let rec find_definitions results output =
+      match results with
+      | [Terminator expr] -> output, expr
+      | [] -> Misc.fatal_error "Missing Terminator"
+      | result::results ->
+        match Node.Map.find result definitions with
+        | exception Not_found ->
+          (* This is ok; the input expression to this pass may not be
+             closed. *)
+          (* CR mshinwell: maybe we should tighten this up *)
+          find_definitions results output
+        | thing_to_lift ->
+          find_definitions results (thing_to_lift :: output)
     in
-    acc, lift body
-*)
-  | Let_cont { name; body; handler = Handler {
-      params; recursive; handler; }; } ->
-    (* The continuation [handler] cannot be lifted---but there might be
-       sequences of [Let_cont]s or [Let]s inside [handler] that can be lifted
-       (without bringing them out of [handler]). *)
-    let handler = lift handler in
-    let acc =
-      (Continuation (name, Handler { params; recursive; handler; })) :: acc
-    in
-    acc, lift body
-  | Let_cont { name; body; handler = Alias alias_to; } ->
-    (* Similar to the previous case, but there's no handler. *)
-    let acc = (Continuation (name, Alias alias_to)) :: acc in
-    acc, lift body
-  | Let { var; defining_expr; body; _ } ->
-    (* Same as the previous case. *)
-    let acc = (Let (var, defining_expr)) :: acc in
-    acc, lift body
-    (* Same as the previous case. *)
-  | Apply _ | Apply_cont _ | Switch _ ->
-    (* These things have no subexpressions, so we're done. *)
-    acc, expr
+    find_definitions results []
+
+let rec build_graph_and_extract_constants expr =
+  let rec build (expr : Flambda.expr) ~graph ~constants
+        ~most_recent_computational_let =
+    match expr with
+  | Let { var; defining_expr; body; } ->
+    begin match defining_expr with
+    | Const _ | Symbol _ ->
+      let constants = Variable.Map.add var defining_expr constants in
+      build expr ~graph ~constants ~most_recent_computational_let
+    | Var _ | Prim _ | Assign _ | Read_mutable _ | Read_symbol_field _
+    | Allocated_const _ | Set_of_closures _ | Project_closure _
+    | Move_within_set_of_closures _ | Project_var _ | Proved_unreachable ->
+      let fvs_defining_expr = Flambda.free_variables_named defining_expr in
+      (* There are no free continuations in the defining expression of a let. *)
+      let graph =
+        add_to_graph graph (Let (var, defining_expr))
+          ~depends_on:(Node.set_of_variable_set fvs_defining_expr)
+      in
+      build expr ~graph ~constants
+        ~most_recent_computational_let:(Node.Set.singleton (Variable var))
+    end
   | Let_mutable { var; initial_value; contents_kind; body; } ->
-    let acc = (Let_mutable (var, initial_value, contents_kind)) :: acc in
-    acc, lift body
+    let graph =
+      add_to_graph graph (Let_mutable (var, initial_value, contents_kind))
+        ~depends_on:(Node.Set.singleton (Variable initial_value))
+    in
+    (* [Let_mutable] doesn't count as a computation. *)
+    build body ~graph ~constants ~most_recent_computational_let
+  | Let_cont { name; body; handler = Alias alias_of; } ->
+    let graph =
+      add_to_graph graph (Let_cont (name, handler))
+        ~depends_on:(Node.Set.singleton (Continuation alias_of))
+    in
+    build body ~graph ~constants ~most_recent_computational_let
+  | Let_cont { name; body; handler =
+      (Handler { params; recursive = _; handler; }) as whole_handler } ->
+    let params = Variable.Set.of_list params in
+    let handler, constants' = lift_returning_constants handler in
+    let constants = Variable.Map.union constants constants' in
+    let add_let_cont_handler_to_graph ~graph ~name ~params ~handler
+          ~whole_handler =
+      let fcs_handler =
+        Continuation.Set.remove Flambda.free_continuations handler name
+      in
+      let fvs_handler =
+        Node.set_of_variable_set (Variable.Set.diff fvs_handler params)
+      in
+      let fcs_handler = Node.set_of_continuation_set fcs_handler in
+      let depends_on = Node.Set.union fvs_handler fcs_handler in
+      add_to_graph graph (Let_cont (name, whole_handler)) ~depends_on
+    in
+    (* Any handler that might be eligible for lifting out of the [Let_cont]
+       must now be at the top of [handler]. *)
+    let rec peel_handlers handler ~graph =
+      match handler with
+      | Let_cont { name = name2; body = body2;
+          handler = (Alias alias_of) as handler; } ->
+        let graph =
+          add_to_graph graph (Let_cont (name2, handler))
+            ~depends_on:(Node.Set.singleton (Continuation alias_of))
+        in
+        peel_handlers body2 ~graph
+      | Let_cont { name = name2; body = body2; handler =
+          (Handler { params = params2; handler = handler2; _ })
+            as whole_handler2 } ->
+        (* This nested handler may be lifted so long as it doesn't use any
+           of the outer handler's parameters. *)
+        let fvs_handler2 = Flambda.free_variables handler2 in
+        if Variable.Set.is_empty (Variable.Set.inter params
+          (Flambda.free_variables handler2))
+        then begin
+          let graph =
+            add_let_cont_handler_to_graph ~graph ~name:name2
+              ~params:(Variable.Set.of_list params2) ~handler:handler2
+              ~whole_handler:whole_handler2
+          in
+          peel_handlers body2 ~graph
+        end else begin
+          (* There's nothing else eligible for lifting in this nested
+             handler. *)
+          graph, handler
+        end
+      | Let_cont _ | Let _ | Let_mutable _
+      | Apply _ | Apply_cont _ | Switch _ -> graph, handler
+    in
+    let graph, handler = peel_handlers handler ~graph in
+      add_to_graph graph
+    in
+    let graph =
+      add_let_cont_handler_to_graph ~graph ~name ~params ~handler
+        ~whole_handler
+    in
+    build body ~graph ~constants ~most_recent_computational_let
+  | Apply _ | Apply_cont _ | Switch _ ->
+    add_to_graph graph (Terminator expr)
+      ~depends_on:most_recent_computational_let
+  in
+  build expr ~constants:Variable.Map.empty
+    ~most_recent_computational_let:Node.Set.empty
 
-and lift (expr : Flambda.t) : Flambda.t =
-  let defs, body = find_things_to_lift [] expr in
-  List.fold_left (fun body (def : thing_to_lift) ->
-      match def with
-      | Continuation (name, handler) ->
-        Flambda.Let_cont { name; body; handler; }
-      | Let (var, Set_of_closures set) ->
-        let set = Flambda_iterators.map_function_bodies set ~f:lift in
-        Flambda.create_let var (Set_of_closures set) body
+and lift_returning_constants (expr : Flambda.t) =
+  let graph, constants = build_graph_and_extract_constants expr in
+  let rev_bindings, terminator = topological_sort graph in
+  List.fold_left (fun body (thing : thing_to_lift) : Flambda.expr ->
+      match thing with
       | Let (var, defining_expr) ->
         Flambda.create_let var defining_expr body
       | Let_mutable (var, initial_value, contents_kind) ->
-        Flambda.Let_mutable { var; initial_value; contents_kind; body; })
-    body
-    defs
+        Let_mutable { var; initial_value; contents_kind; body; }
+      | Let_cont (name, handler) ->
+        Let_cont { name; body; handler; }
+      | Terminator _ -> assert false)
+    terminator
+    rev_bindings
+
+let lift (expr : Flambda.t) =
+  let constants = lift_returning_constants expr in
+  Variable.Map.fold (fun var const expr ->
+      Flambda.create_let var const expr)
+    constants
+    expr
 
 let run program =
   Flambda_iterators.map_exprs_at_toplevel_of_program program ~f:lift
--- middle_end/lift_let_cont.mli
+++ middle_end/lift_let_cont.mli
@@ -21,28 +21,91 @@
     exist in the context of continuations.  Avoiding that would require
     something like converting to SSA form.)
 
-    More specifically, this pass detects situations of the form:
 
-    let_cont k1 x1 =
-      let_cont k2 x2 =
-        <cont>
-      in
-      <body2>
-    in
-    <body1>
+Constructs introducing nesting are: let/let_mutable (just treat this as let)
+  and let_cont.  So basically all we have to handle is let vs. let_cont in
+  both body and handler positions.
+Terminators are: apply, apply_cont, switch.
 
-    (with [x1] not free in <cont>) and transform them to:
+We always want to move let_cont as high as possible.
+Lets that have computational cost should never be lifted.
 
-    let_cont k2 x2 =
-      <cont>
-    in
-    let_cont k1 x1 =
-      <body2>
-    in
-    <body1>
+Maybe it should be: any let that binds a constant should be lifted right
+to the top.  Then you just have to consider cases where the let_cont
+(really, a block of let_conts) is/are second:
 
-    (This example is simplified since it supposes continuations only have
-    one argument.)
+1.
+  let x = (* computation *) in
+  let_cont k1 = (* not using x *) in
+  ...
+  let_cont kn = (* not using x *) in
+-->
+  let_cont k1 = (* not using x *) in
+  ...
+  let_cont kn = (* not using x *) in
+  let x = (* computation *) in
+
+2.
+  let_cont k1 = ... in
+  let_cont k2 = (* not using k1 *) in
+  ...
+  let_cont kn = (* not using k1 *) in
+-->
+  let_cont k2 = (* not using k1 *) in
+  ...
+  let_cont kn = (* not using k1 *) in
+  let_cont k1 = ... in
+
+The final thing is to deal with handlers.  When entering a handler the
+first thing is to apply rules 1 and 2 above.  This means in particular that
+any continuation eligible for lifting out save perhaps that it may depend on
+one of the handler's arguments will be at the top.  Thus only one more rule
+is needed, applied recursively to the rewritten handler:
+3.
+  let_cont k1 xs =
+                                                   ]
+    let_cont k2 = (* not involving xs *) in body2  ] this is the rewritten
+                                                   ] handler
+  in
+  body1
+--->
+  let_cont k2 = (* not involving xs *) in body2
+  let_cont k1 xs = body2 in
+  body1
+
+Ignoring terminators:
+
+- Lets that bind constants depend on "top" only
+- Lets that bind anything else depend on the previous "let" or if not "top"
+
+let x = (* computation *) in
+let k1 = (* not using x *) in
+let y = ... x ... in
+let z = (* computation *) in
+let k2 = ... x ... in
+let k3 = ... k2 ... in
+let k4 = (* uses x but not y *) in
+
+(x = ..., {}, {})
+(k1 = ..., {}, {})
+(y = ..., {x}, {})
+(z = ..., {y}, {})  (* note that for [z] we add [y] as a dependency *)
+(k2 = ..., {x}, {})
+(k3 = ..., {}, {k2})
+(k4 = ..., {x}, {})
+
+                  top
+                   |
+        ----------------------
+        |                    |
+        |                    |
+        x                    k1
+  ------|--------------
+  |          |        |
+  y          k2       k4
+  |          |
+  z          k3
+
 *)
 
 [@@@ocaml.warning "+a-4-9-30-40-41-42"]
