b81c8bcb79b36d040f0c005ecd8ef7fe01c13252
Mark Shinwell <mshinwell@janestreet.com>
2016-11-17 14:27:56 +0000

    sink

--- middle_end/middle_end.ml
+++ middle_end/middle_end.ml
@@ -109,6 +109,7 @@ let middle_end ppf ~source_provenance ~prefixname ~backend
       let round = 0 in
       flam
       +-+ ("Lift_let_cont 1", Lift_let_cont.run)
+      +-+ ("Sink_lets 1", Sink_lets.run)
       +-+ ("Lift_constants", Lift_constants.lift_constants ~backend)
       +-+ ("Share_constants", Share_constants.share_constants)
       +-+ ("Lift_let_to_initialize_symbol",
@@ -134,6 +135,7 @@ let middle_end ppf ~source_provenance ~prefixname ~backend
         (* Beware: [Lift_constants] must be run before any pass that might
            duplicate strings. *)
         +-+ ("Lift_let_cont 1", Lift_let_cont.run)
+        +-+ ("Sink_lets 1", Sink_lets.run)
         +-+ ("Lift_constants", Lift_constants.lift_constants ~backend)
         +-+ ("Share_constants", Share_constants.share_constants)
         +-+ ("Remove_unused_program_constructs",
@@ -141,6 +143,7 @@ let middle_end ppf ~source_provenance ~prefixname ~backend
         +-+ ("Lift_let_to_initialize_symbol",
              Lift_let_to_initialize_symbol.lift ~backend)
         +-+ ("Lift_let_cont 2", Lift_let_cont.run)
+        +-+ ("Sink_lets 2", Sink_lets.run)
         +-+ ("Remove_unused_closure_vars 1",
              Remove_unused_closure_vars.remove_unused_closure_variables
               ~remove_direct_call_surrogates:false)
@@ -151,6 +154,7 @@ let middle_end ppf ~source_provenance ~prefixname ~backend
              Remove_unused_closure_vars.remove_unused_closure_variables
               ~remove_direct_call_surrogates:false)
         +-+ ("Lift_let_cont 3", Lift_let_cont.run)
+        +-+ ("Sink_lets 3", Sink_lets.run)
         +-+ ("Ref_to_variables",
              Ref_to_variables.eliminate_ref)
         +-+ ("Inline_and_simplify noinline",
--- middle_end/sink_lets.ml
+++ middle_end/sink_lets.ml
@@ -13,3 +13,47 @@
 (*   special exception on linking described in the file LICENSE.          *)
 (*                                                                        *)
 (**************************************************************************)
+
+[@@@ocaml.warning "+a-4-9-30-40-41-42"]
+
+module W = Flambda.With_free_variables
+
+module State = struct
+  type t = {
+    to_sink : (Variable.t * Flambda.named W.t) Continuation.Map.t;
+    
+  }
+end
+
+let rec sink_expr (expr : Flambda.expr) ~state =
+  match expr with
+  | Let ({ var; defining_expr; body; } as let_expr) ->
+    let body, state = sink_expr body ~state in
+    let fvs =
+      Variable.Set.union (Flambda.free_variables_named defining_expr)
+        (Variable.Set.remove var (Flambda.free_variables body))
+    in
+
+  | Let_mutable { var; initial_value; contents_kind; body; } ->
+
+  | Let_cont { name; body; handler = (Alias alias_of) as handler; } ->
+
+  | Let_cont { name; body; handler =
+      Handler { params; recursive; handler; } } ->
+    let body, state = sink_expr body ~state in
+    let handler, handler_state = sink_expr handler ~state:(State.create ()) in
+    let fvs_handler =
+      Variable.Set.diff (Flambda.free_variables handler)
+        (Variable.Set.of_list params)
+    in
+    let candidates_to_be_sunk =
+      Variable.Set.diff fvs_handler (Flambda.free_variables body)
+    in
+
+
+  | Apply _ | Apply_cont _ | Switch _ ->
+
+and sink (expr : Flambda.t) =
+
+let run program =
+  Flambda_iterators.map_exprs_at_toplevel_of_program program ~f:sink
