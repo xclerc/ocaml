5214597b603f1b304d13578d828b986beffd4abc
Mark Shinwell <mshinwell@janestreet.com>
2017-02-02 12:26:07 +0000

    stuff

--- middle_end/unbox_returns.ml
+++ middle_end/unbox_returns.ml
@@ -86,6 +86,7 @@ let wrapper_returning_boxed_result ~params ~new_fun_var ... =
 
 let unbox_function_decl ~fun_var ~(function_decl : Flambda.function_declaration)
       ~how_to_unbox ~return_cont_param =
+  let dbg = Debuginfo.none in
   let new_fun_var = Variable.rename fun_var in
   (* There are two steps:
      1. Build a stub continuation that is called by the function instead of
@@ -138,13 +139,52 @@ let unbox_function_decl ~fun_var ~(function_decl : Flambda.function_declaration)
       ~is_a_functor:function_decl.is_a_functor
   in
   let new_fun_var = Variable.rename fun_var in
-  let stub_return_cont = Continuation.create () in
+  let wrapper_return_cont = Continuation.create () in
+  let _fun_wrapper_params_map, fun_wrapper_params,
+      fun_wrapper_specialised_args =
+    Flambda_utils.create_wrapper_params ~params:function_decl.params
+      ~specialised_args:function_decl.specialised_args
+      ~freshening_already_assigned:Variable.Map.empty
+  in
+  let function_stub_body : Flambda.expr =
+    let receive_results = Continuation.create () in
+    let results =
+      Array.to_list (Array.create (List.length how_to_unbox.params)
+        (fun index ->
+          let name = Printf.sprintf "result%d" index in
+          Variable.create name))
+    in
+    let box_results_and_call_return_cont : Flambda.expr =
 
+    in
+    Let_cont {
+      body = Apply {
+        kind = Function;
+        func = new_fun_var;
+        continuation = receive_results;
+        args = fun_wrapper_params;
+        call_kind = Direct (Closure_id.wrap new_fun_var);
+        dbg;
+        inline = Lambda.Default_inline;
+        specialise = Lambda.Default_specialise;
+      };
+      handler = Nonrecursive {
+        name = receive_results;
+        handler = {
+          params = results;
+          handler = box_results_and_call_return_cont;
+          stub = false;
+          is_exn_handler = false;
+          specialised_args = Variable.Map.empty;
+        };
+      };
+    };
+  in
   let function_stub_decl =
-    Flambda.create_function_declaration ~continuation_param:stub_return_cont
-      ~params:function_decl.params
-      ~body:new_function_body
-      ~stub:function_decl.stub
+    Flambda.create_function_declaration ~continuation_param:wrapper_return_cont
+      ~params:fun_wrapper_params
+      ~body:function_stub_body
+      ~stub:true
       ~dbg:function_decl.dbg
       ~inline:function_decl.inline
       ~specialise:function_decl.specialise
@@ -152,7 +192,8 @@ let unbox_function_decl ~fun_var ~(function_decl : Flambda.function_declaration)
   in
   Variable.Map.add new_fun_var function_decl
     (Variable.Map.add fun_var function_stub_decl
-      Variable.Map.empty)
+      Variable.Map.empty),
+    fun_wrapper_specialised_args
 
 let for_function_decl r ~backend:_ ~fun_var
         ~(function_decl : Flambda.function_declaration) =
@@ -178,13 +219,14 @@ let for_function_decl r ~backend:_ ~fun_var
         match how_to_unbox with
         | None -> None
         | Some how_to_unbox ->
-          let function_decls =
+          let function_decls, new_specialised_args =
             unbox_function_decl ~fun_var ~function_decl ~how_to_unbox
               ~return_cont_param
           in
-          Some function_decls
+          Some (function_decls, new_specialised_args)
 
 let run r ... =
+
       let function_decls =
         Flambda.update_function_declarations set_of_closures.function_decls
           ~funs
