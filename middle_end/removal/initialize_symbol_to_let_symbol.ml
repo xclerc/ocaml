33f044ae819ed411bad0557ea37930cdd4fad6af
Mark Shinwell <mshinwell@gmail.com>
2017-10-16 16:18:09 +0100

    fixes

--- middle_end/removal/initialize_symbol_to_let_symbol.ml
+++ middle_end/removal/initialize_symbol_to_let_symbol.ml
@@ -5,8 +5,8 @@
 (*                       Pierre Chambart, OCamlPro                        *)
 (*           Mark Shinwell and Leo White, Jane Street Europe              *)
 (*                                                                        *)
-(*   Copyright 2013--2016 OCamlPro SAS                                    *)
-(*   Copyright 2014--2016 Jane Street Group LLC                           *)
+(*   Copyright 2013--2017 OCamlPro SAS                                    *)
+(*   Copyright 2014--2017 Jane Street Group LLC                           *)
 (*                                                                        *)
 (*   All rights reserved.  This file is distributed under the terms of    *)
 (*   the GNU Lesser General Public License version 2.1, with the          *)
@@ -14,36 +14,42 @@
 (*                                                                        *)
 (**************************************************************************)
 
+(* XXX mshinwell: Going to see if Simplify can do this, should be easier
+for the multi-argument continuation case of Initialize_symbol, since the
+types can tell us what constants reach which continuations *)
+
+(*
 [@@@ocaml.warning "+a-4-9-30-40-41-42"]
 
-let constant_field ((expr : Flambda.Expr.t), cont)
-  : Flambda_static.Constant_defining_value.t_block_field option =
+module BF = Flambda_static.Constant_defining_value_block_field
+
+let constant_expr (expr : Flambda.Expr.t) cont =
   match expr with
-  | Let { var; defining_expr = Const c;
+  | Let { var; defining_expr = Const (Tagged_immediate imm);
         body = Apply_cont (cont', None, [var']); _ }
       when Continuation.equal cont cont' ->
+    (* CR mshinwell: This needs a more complicated analysis to cope with
+       the case where the continuation has multiple arguments. *)
     assert (Variable.equal var var');
     (* This must be true since var is the only variable in scope *)
-    Some (Flambda.Const c)
+    Some [BF.Tagged_immediate imm]
   | Let { var; defining_expr = Symbol s;
         body = Apply_cont (cont', None, [var']); _ }
       when Continuation.equal cont cont' ->
     assert (Variable.equal var var');
-    Some (Flambda.Symbol s)
+    Some [BF.Symbol s]
   | _ ->
     None
 
-let rec loop (program : Flambda_static.Program.t_body) : Flambda_static.Program.t_body =
+let rec loop (program : Flambda_static.Program_body.t)
+      : Flambda_static.Program_body.t =
   match program with
-  | Initialize_symbol (symbol, tag, fields, program) ->
-    let constant_fields = List.map constant_field fields in
-    begin
-      match Misc.Stdlib.List.some_if_all_elements_are_some constant_fields
-    with
+  | Initialize_symbol (symbol, descr, program) ->
+    begin match constant_expr descr.expr descr.return_cont with
     | None ->
-      Initialize_symbol (symbol, tag, fields, loop program)
+      Initialize_symbol (symbol, descr, loop program)
     | Some fields ->
-      Let_symbol (symbol, Block (tag, fields), loop program)
+      Let_symbol (symbol, Block (descr.tag, fields), loop program)
     end
   | Let_symbol (symbol, const, program) ->
     Let_symbol (symbol, const, loop program)
@@ -58,3 +64,7 @@ let run (program : Flambda_static.Program.t) =
   { program with
     program_body = loop program.program_body;
   }
+
+*)
+
+let run (program : Flambda_static.Program.t) = program
--- middle_end/removal/ref_to_variables.ml
+++ middle_end/removal/ref_to_variables.ml
@@ -5,8 +5,8 @@
 (*                       Pierre Chambart, OCamlPro                        *)
 (*           Mark Shinwell and Leo White, Jane Street Europe              *)
 (*                                                                        *)
-(*   Copyright 2013--2016 OCamlPro SAS                                    *)
-(*   Copyright 2014--2016 Jane Street Group LLC                           *)
+(*   Copyright 2013--2017 OCamlPro SAS                                    *)
+(*   Copyright 2014--2017 Jane Street Group LLC                           *)
 (*                                                                        *)
 (*   All rights reserved.  This file is distributed under the terms of    *)
 (*   the GNU Lesser General Public License version 2.1, with the          *)
@@ -41,7 +41,8 @@ let variables_not_used_as_local_reference (tree:Flambda.Expr.t) =
       set := Variable.Set.union !set (Flambda.Named.free_variables flam)
     | Set_of_closures set_of_closures ->
       set := Variable.Set.union !set (Flambda.Named.free_variables flam);
-      Variable.Map.iter (fun _ (function_decl : Flambda.Function_declaration.t) ->
+      Closure_id.Map.iter
+        (fun _ (function_decl : Flambda.Function_declaration.t) ->
           loop function_decl.body)
         set_of_closures.function_decls.funs
     | Assign _ ->
@@ -76,9 +77,20 @@ let variables_containing_ref (flam:Flambda.Expr.t) =
   let aux (flam : Flambda.Expr.t) =
     match flam with
     | Let { var;
-            defining_expr = Prim(Pmakeblock(0, Asttypes.Mutable, _), l, _);
+            kind;
+            defining_expr =
+              Prim (Pmakeblock (0, Asttypes.Mutable, shape), l, _);
           } ->
-      map := Variable.Map.add var (List.length l) !map
+      if not (Flambda_kind.is_value kind) then begin
+        Misc.fatal_errorf "Variable %a bound to [Pmakeblock] but has kind \
+            %a"
+          Variable.print var
+          Flambda_kind.print kind
+      end;
+      let arity =
+        Flambda_arity.of_block_shape shape ~num_fields:(List.length l)
+      in
+      map := Variable.Map.add var arity !map
     | _ -> ()
   in
   Flambda.Expr.Iterators.iter aux (fun _ -> ()) flam;
@@ -89,88 +101,112 @@ let eliminate_ref_of_expr flam =
     variables_not_used_as_local_reference flam
   in
   let convertible_variables =
-    Variable.Map.filter
-      (fun v _ ->
+    Variable.Map.filter (fun v _arity ->
         not (Variable.Set.mem v variables_not_used_as_local_reference))
       (variables_containing_ref flam)
   in
   if Variable.Map.cardinal convertible_variables = 0 then flam
   else
     let convertible_variables =
-      Variable.Map.mapi (fun v size ->
-          Array.init size (fun _ -> rename_var v))
+      Variable.Map.mapi (fun v arity ->
+          Array.of_list (List.map (fun kind -> rename_var v, kind) arity))
         convertible_variables
     in
     let convertible_variable v = Variable.Map.mem v convertible_variables in
     let get_variable v field =
-      let arr = try Variable.Map.find v convertible_variables
-        with Not_found -> assert false in
+      let arr =
+        try Variable.Map.find v convertible_variables
+        with Not_found -> assert false
+      in
       if Array.length arr <= field
-      then None (* This case could apply when inlining code containing GADTS *)
-      else Some (arr.(field), Array.length arr)
+      then None (* This case could apply when inlining code containing GADTs *)
+      else Some (fst arr.(field), snd arr.(field), Array.length arr)
     in
-    let aux_named var (named : Flambda.Named.t) =
+    let aux_named var kind (named : Flambda.Named.t) =
+      let module R = Flambda.Reachable in
       match named with
-      | Prim(Pfield field, [v], _)
-        when convertible_variable v ->
-        (match get_variable v field with
-         | None -> (), [], var, Flambda.Unreachable
-         | Some (var',_) -> (), [], var, Flambda.Reachable (Read_mutable var'))
-      | Prim(Poffsetref delta, [v], dbg)
-        when convertible_variable v ->
-        (match get_variable v 0 with
-         | None -> (), [], var, Flambda.Unreachable
-         | Some (var', size) ->
-           if size = 1
-           then begin
-             let mut = Variable.create "read_mutable" in
-             let new_value = Variable.create "offseted" in
-             (), [
-               mut, Flambda.Read_mutable var';
-               new_value, Flambda.Prim (Poffsetint delta, [mut], dbg);
-             ], var,
-               Flambda.Reachable (Assign { being_assigned = var'; new_value; })
-           end
-           else
-             (), [], var, Flambda.Unreachable)
-      | Prim(Psetfield (field, _, _), [v; new_value], _)
-        when convertible_variable v ->
-        (match get_variable v field with
-         | None -> (), [], var, Flambda.Unreachable
-         | Some (being_assigned,_) ->
-           (), [], var,
-             Flambda.Reachable (Assign { being_assigned; new_value }))
+      | Prim (Pfield field, [v], _)
+          when convertible_variable v ->
+        begin match get_variable v field with
+        | None -> (), [], var, kind, R.Unreachable
+        | Some (var', kind', _size) ->
+          assert (Flambda_kind.equal kind kind');
+          (), [], var, kind, R.Reachable (Read_mutable var')
+        end
+      | Prim (Poffsetref delta, [v], dbg)
+          when convertible_variable v ->
+        assert (Flambda_kind.is_value kind);
+        begin match get_variable v 0 with
+        | None -> (), [], var, kind, R.Unreachable
+        | Some (var', kind', size) ->
+          if size = 1
+          then begin
+            let mut = Variable.create "read_mutable" in
+            let new_value = Variable.create "offseted" in
+            let new_bindings = [
+              mut, kind',
+                Flambda.Named.Read_mutable var';
+              new_value, kind',
+                Flambda.Named.Prim (Poffsetint delta, [mut], dbg);
+            ] in
+            (), new_bindings, var, kind,
+              R.Reachable (Assign { being_assigned = var'; new_value; })
+          end
+          else
+            (), [], var, kind, R.Unreachable
+        end
+      | Prim (Psetfield (field, _, _), [v; new_value], _)
+          when convertible_variable v ->
+        assert (Flambda_kind.is_value kind);
+        begin match get_variable v field with
+        | None -> (), [], var, kind, R.Unreachable
+        | Some (being_assigned, _kind, _size) ->
+          (), [], var, kind,
+            R.Reachable (Assign { being_assigned; new_value })
+        end
       | Prim _ | Var _ | Symbol _ | Const _ | Allocated_const _ | Read_mutable _
       | Read_symbol_field _ | Set_of_closures _ | Project_closure _
       | Move_within_set_of_closures _ | Project_var _ | Assign _ ->
-        (), [], var, Flambda.Reachable named
+        (), [], var, kind, R.Reachable named
     in
     let aux (flam : Flambda.Expr.t) : Flambda.Expr.t =
       match flam with
-      | Let { var;
-              defining_expr = Prim(Pmakeblock(0, Asttypes.Mutable, shape), l,_);
-              body }
-        when convertible_variable var ->
-        let shape = match shape with
-          | None -> List.map (fun _ -> Lambda.Pgenval) l
-          | Some shape -> shape
+      | Let {
+          var;
+          kind;
+          defining_expr = Prim (Pmakeblock (0, Asttypes.Mutable, shape), l, _);
+          body;
+        }
+          when convertible_variable var ->
+        if not (Flambda_kind.is_value kind) then begin
+          Misc.fatal_errorf "Variable %a bound to [Pmakeblock] but has kind \
+              %a"
+            Variable.print var
+            Flambda_kind.print kind
+        end;
+        let arity =
+          Flambda_arity.of_block_shape shape ~num_fields:(List.length l)
         in
         let _, expr =
-          List.fold_left2 (fun (field,body) init kind ->
+          List.fold_left2 (fun (field, body) initial_value kind ->
               match get_variable var field with
               | None -> assert false
-              | Some (field_var, _) ->
-                field+1,
-                (Let_mutable { var = field_var;
-                               initial_value = init;
-                               body;
-                               contents_kind = kind } : Flambda.Expr.t))
-            (0, body) l shape in
+              | Some (field_var, _kind, _size) ->
+                field + 1,
+                  (Let_mutable {
+                    var = field_var;
+                    initial_value;
+                    body;
+                    contents_type = Flambda_type.unknown kind Other;
+                  } : Flambda.Expr.t))
+            (0, body) l arity in
         expr
       | Let _ ->
         let flam, () =
-          Flambda.fold_lets_option flam ~init:()
-            ~for_defining_expr:(fun () var named -> aux_named var named)
+          Flambda.Expr.Folders.fold_lets_option flam
+            ~init:()
+            ~for_defining_expr:(fun () var kind named ->
+              aux_named var kind named)
             ~for_last_body:(fun () expr -> expr, ())
             ~filter_defining_expr:(fun () var named _ ->
               (), var, Some named)
