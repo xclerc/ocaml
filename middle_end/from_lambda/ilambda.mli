35b1a181149809c4795594a0bc5dc83e2a7c8890
Pierre Chambart <pierre.chambart@ocamlpro.com>
2017-11-20 18:08:15 +0100

    Update cps_conversion for exn continuation

--- middle_end/from_lambda/ilambda.mli
+++ middle_end/from_lambda/ilambda.mli
@@ -50,6 +50,7 @@ and let_mutable = {
 and function_declaration =
   { kind : Lambda.function_kind;
     continuation_param : Continuation.t;
+    exn_continuation_param : Continuation.t;
     params : (Ident.t * Lambda.value_kind) list;
     return : Lambda.value_kind;
     body : t;
@@ -78,6 +79,7 @@ and apply =
     func : Ident.t;
     args : Ident.t list;
     continuation : Continuation.t;
+    exn_continuation : Continuation.t;
     loc : Location.t;
     should_be_tailcall : bool;
     inlined : Lambda.inline_attribute;
@@ -94,6 +96,12 @@ and switch =
   { numconsts : int;
     consts : (int * Continuation.t) list;
     failaction : Continuation.t option;
+  }
+
+type program =
+  { expr : t;
+    return_continuation : Continuation.t;
+    exception_continuation : Continuation.t;
   }
 
 val print : Format.formatter -> t -> unit
