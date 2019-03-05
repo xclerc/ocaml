e9b36007480e5c280d2aef84811b4a6ce1346d26
Xavier Clerc <xclerc@janestreet.com>
2019-03-05 14:26:29 +0000

    Add comment.

--- testsuite/tests/tool-debugger/printer/printer.ml
+++ testsuite/tests/tool-debugger/printer/printer.ml
@@ -1,4 +1,5 @@
 let p : Format.formatter -> int -> unit = fun fmt n ->
+  (* Ensure that printers can use modules from the debugger. *)
   for _i = 1 to min n !Printval.max_printer_depth do
     Format.pp_print_string fmt "S ";
   done;
