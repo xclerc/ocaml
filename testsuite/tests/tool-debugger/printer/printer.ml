c011994fef28fb2adbfd657ad0efe729abcb15ab
Xavier Clerc <xclerc@janestreet.com>
2019-03-06 10:26:33 +0000

    Better comment.

--- testsuite/tests/tool-debugger/printer/printer.ml
+++ testsuite/tests/tool-debugger/printer/printer.ml
@@ -1,5 +1,7 @@
 let p : Format.formatter -> int -> unit = fun fmt n ->
-  (* Ensure that printers can use modules from the debugger. *)
+  (* We use `max_printer_depth` to tweak the output so that
+     this test shows that the printer not only compiles
+     against the debugger's code, but also uses its state. *)
   for _i = 1 to min n !Printval.max_printer_depth do
     Format.pp_print_string fmt "S ";
   done;
