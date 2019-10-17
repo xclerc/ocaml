5cbc20b2087983d5005a30c26fc8f64e1d15d4e9
Mark Shinwell <mshinwell@gmail.com>
2019-10-17 14:16:47 +0100

    Revert PR#8672, which causes ill-typed Lambda code

--- lambda/switch.ml
+++ lambda/switch.ml
@@ -660,8 +660,9 @@ let rec pkey chan  = function
           and right = {s with cases=right} in
 
           if i=1 && (lim+ctx.off)=1 && get_low cases 0+ctx.off=0 then
-            Arg.make_if
-              ctx.arg
+            (* CR mshinwell (with trefis): This is related to PR#8672. *)
+            make_if_ne (* Arg.make_if *)
+              ctx.arg 0
               (c_test ctx right) (c_test ctx left)
           else if less_tests cright cleft then
             make_if_lt
