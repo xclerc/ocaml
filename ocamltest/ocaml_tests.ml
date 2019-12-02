295248753ab2f42210c877d146bbe850dd8f799e
Guillaume Bury <guillaume.bury@gmail.com>
2019-12-02 15:59:41 +0100

    Add Github actions workflow + some small things
    Additionally:
    
    Revert "don't use local cvt_emit and make_opcodes"
    This reverts commit 21ca3fab54dc10eda7469f38f29c63975a15c506.

--- ocamltest/ocaml_tests.ml
+++ ocamltest/ocaml_tests.ml
@@ -22,12 +22,10 @@ open Ocaml_actions
 let bytecode =
   let opt_actions =
   [
-(*
     setup_ocamlc_opt_build_env;
     ocamlc_opt;
     check_ocamlc_opt_output;
     compare_bytecode_programs
-*)
   ] in
 {
   test_name = "bytecode";
@@ -50,12 +48,10 @@ let native =
     check_ocamlopt_byte_output;
     run;
     check_program_output;
-(*
     setup_ocamlopt_opt_build_env;
     ocamlopt_opt;
     check_ocamlopt_opt_output;
     compare_native_programs;
-*)
   ] in
   {
     test_name = "native";
