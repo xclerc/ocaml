41b3c7d573d1ce88390175801ef5e41312f21657
Mark Shinwell <mshinwell@gmail.com>
2019-12-11 12:16:02 +0000

    Extension ctor blocks must be Mutable

--- lambda/translcore.ml
+++ lambda/translcore.ml
@@ -61,7 +61,7 @@ let transl_extension_constructor ~scopes env path ext =
   let loc = of_raw_location ~scopes ext.ext_loc in
   match ext.ext_kind with
     Text_decl _ ->
-      Lprim (Pmakeblock (Obj.object_tag, Immutable, None),
+      Lprim (Pmakeblock (Obj.object_tag, Mutable, None),
         [Lconst (Const_base (Const_string (name, None)));
          Lprim (prim_fresh_oo_id, [Lconst (Const_base (Const_int 0))], loc)],
         loc)
