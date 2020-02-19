1a496d41c9e2766769b616e20ac36a681c21e1dc
Pierre Chambart <pierre.chambart@ocamlpro.com>
2020-02-19 20:06:55 +0100

    Fix Closures_entry meet and join

--- middle_end/flambda2.0/types/structures/closures_entry.rec.ml
+++ middle_end/flambda2.0/types/structures/closures_entry.rec.ml
@@ -22,10 +22,13 @@ module PV = Product.Var_within_closure_indexed
 module TEE = Typing_env_extension
 
 type t = {
+  (* CR pchambart: This is exactly a Product with a different kind of fields
+     Product should maybe be functorized once more to represent this.
+     Maybe not because product also contains a kind which we don't want in that case.
+
+     Also the closure_types and closure_var_types products shouldn't have to store the
+     kind because it is always the same. *)
   function_decls : FDT.t Closure_id.Map.t;
-  (* CR pchambart: this probably needs also a list of available
-     closure_var. Product does not provides that information.
-     If something is not represented it is unknown and not bottom *)
   closure_types : PC.t;
   closure_var_types : PV.t;
 }
@@ -35,17 +38,6 @@ let create ~function_decls ~closure_types ~closure_var_types =
     closure_types;
     closure_var_types;
   }
-
-let create_bottom () =
-  { function_decls = Closure_id.Map.empty;
-    closure_types = PC.create_top Flambda_kind.value;
-    closure_var_types = PV.create_top Flambda_kind.value;
-  }
-
-let is_bottom { function_decls; closure_types = _; closure_var_types = _; } =
-  Closure_id.Map.is_empty function_decls
-    (* && PC.is_bottom closure_types
-     * && PV.is_bottom closure_var_types *)
 
 let print_with_cache ~cache ppf
       { function_decls; closure_types; closure_var_types; } =
@@ -61,75 +53,77 @@ let print_with_cache ~cache ppf
 
 let print ppf t = print_with_cache ~cache:(Printing_cache.create ()) ppf t
 
-module Make_meet_or_join
-  (E : Lattice_ops_intf.S
-   with type meet_env := Meet_env.t
-   with type meet_or_join_env := Meet_or_join_env.t
-   with type typing_env := Typing_env.t
-   with type typing_env_extension := Typing_env_extension.t) =
-struct
-  let meet_or_join env
-        { function_decls = function_decls1;
-          closure_types = closure_types1;
-          closure_var_types = closure_var_types1;
-        }
-        { function_decls = function_decls2;
-          closure_types = closure_types2;
-          closure_var_types = closure_var_types2;
-        } : _ Or_bottom.t =
-    let env_extensions = ref [] in
-    let function_decls =
-      Closure_id.Map.merge (fun _closure_id func_decl1 func_decl2 ->
-          match func_decl1, func_decl2 with
-          | None, None | Some _, None | None, Some _ -> None
-          | Some func_decl1, Some func_decl2 ->
-            match E.switch FDT.meet FDT.join env func_decl1 func_decl2 with
-            | Bottom -> None
-            | Ok (func_decl, env_extension) ->
-              env_extensions := env_extension :: !env_extensions;
-              Some func_decl)
-        function_decls1 function_decls2
-    in
-    if Closure_id.Map.is_empty function_decls then Bottom
-    else
-      Or_bottom.both
-        (E.switch PC.meet PC.join env closure_types1 closure_types2)
-        (E.switch PV.meet PV.join env closure_var_types1 closure_var_types2)
-        ~f:(fun (closure_types, env_extension1)
-                (closure_var_types, env_extension2) ->
-          let closures_entry =
-            { function_decls;
-              closure_types;
-              closure_var_types;
-            }
-          in
-          let env_extension =
-            match E.op () with
-            | Join -> TEE.empty ()
-            | Meet ->
-              TEE.n_way_meet (Meet_or_join_env.meet_env env)
-                (env_extension1 :: env_extension2 :: !env_extensions)
-          in
-          closures_entry, env_extension)
-end
+let meet env
+      { function_decls = function_decls1;
+        closure_types = closure_types1;
+        closure_var_types = closure_var_types1;
+      }
+      { function_decls = function_decls2;
+        closure_types = closure_types2;
+        closure_var_types = closure_var_types2;
+      } : _ Or_bottom.t =
+  let any_bottom = ref false in
+  let env_extensions = ref [] in
+  let function_decls =
+    Closure_id.Map.merge (fun _closure_id func_decl1 func_decl2 ->
+      match func_decl1, func_decl2 with
+      | None, None -> None
+      | Some func_decl, None | None, Some func_decl -> Some func_decl
+      | Some func_decl1, Some func_decl2 ->
+        match FDT.meet env func_decl1 func_decl2 with
+        | Bottom ->
+          any_bottom := true;
+          None
+        | Ok (func_decl, env_extension) ->
+          env_extensions := env_extension :: !env_extensions;
+          Some func_decl)
+      function_decls1 function_decls2
+  in
+  if !any_bottom then
+    Bottom
+  else
+    Or_bottom.both
+      (PC.meet env closure_types1 closure_types2)
+      (PV.meet env closure_var_types1 closure_var_types2)
+      ~f:(fun (closure_types, env_extension1)
+              (closure_var_types, env_extension2) ->
+           let closures_entry =
+             { function_decls;
+               closure_types;
+               closure_var_types;
+             }
+           in
+           let env_extension =
+             TEE.n_way_meet env
+               (env_extension1 :: env_extension2 :: !env_extensions)
+           in
+           closures_entry, env_extension)
 
-module Meet = Make_meet_or_join (Lattice_ops.For_meet)
-module Join = Make_meet_or_join (Lattice_ops.For_join)
-
-let meet env t1 t2 : _ Or_bottom.t =
-  let env = Meet_or_join_env.create_for_meet env in
-  match Meet.meet_or_join env t1 t2 with
-  | Bottom -> Bottom
-  | Ok (t, env_extension) ->
-    if is_bottom t then Bottom
-    else Ok (t, env_extension)
-
-let join env t1 t2 =
-  match Join.meet_or_join env t1 t2 with
-  | Ok (t, env_extension) ->
-    assert (TEE.is_empty env_extension);
-    t
-  | Bottom -> create_bottom ()
+let join env
+      { function_decls = function_decls1;
+        closure_types = closure_types1;
+        closure_var_types = closure_var_types1;
+      }
+      { function_decls = function_decls2;
+        closure_types = closure_types2;
+        closure_var_types = closure_var_types2;
+      } =
+  let function_decls =
+    Closure_id.Map.merge (fun _closure_id func_decl1 func_decl2 ->
+      match func_decl1, func_decl2 with
+      | None, None
+      | Some _, None
+      | None, Some _ -> None
+      | Some func_decl1, Some func_decl2 ->
+        Some (FDT.join env func_decl1 func_decl2))
+      function_decls1 function_decls2
+  in
+  let closure_types = PC.join env closure_types1 closure_types2 in
+  let closure_var_types = PV.join env closure_var_types1 closure_var_types2 in
+  { function_decls;
+    closure_types;
+    closure_var_types;
+  }
 
 let apply_name_permutation
       { function_decls; closure_types; closure_var_types; } perm =
